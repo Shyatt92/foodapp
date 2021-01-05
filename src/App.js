ba8522a6266d5ad2e5080910fc5113eb4f773f4f
Stephen Hyatt <shyatt92@gmail.com>
2021-01-05 15:37:37 +0000

    Added LoginForm component and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import './App.css'
 import BannerHome from './components/BannerHome'
+import LoginForm from './components/LoginForm'
 
 function App() {
   return (
     <div className="App">
       <BannerHome />
+      <LoginForm />
     </div>
   )
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './LoginForm.css'
+
+const LoginForm = () => {
+  return (
+    <div className='loginForm'>
+      <form className='login'>
+        <label htmlFor='email' className='formElement'>Email</label>
+        <input type='email' name='email' id='email' className='formElement' required></input><br/>
+        <label htmlFor='password' className='formElement'>Password</label>
+        <input type='password' name='password' id='password' className='formElement' required></input><br/><br/>
+        <input type='submit' value='Log In' className='formElement submit'></input>
+      </form>
+      <form className='signup'>
+        <label htmlFor='email' className='formElement'>Email</label>
+        <input type='email' name='email' id='email' className='formElement' required></input><br/><br/>
+        <input type='submit' value='Sign Up' className='formElement submit'></input>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm
