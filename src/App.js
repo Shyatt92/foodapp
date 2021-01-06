b082ca8af263a627da63027915fb340a96b2561b
Stephen Hyatt <shyatt92@gmail.com>
2021-01-06 11:42:39 +0000

    Added contact form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import BannerHome from './components/BannerHome'
 import LoginForm from './components/LoginForm'
 import HomePage from './components/HomePage'
 import AboutUs from './components/AboutUs'
+import ContactUs from './components/ContactUs'
 
 function App() {
   const [ loadHomeComponent, setLoadHomeComponent ] = useState('home')
@@ -14,7 +15,7 @@ function App() {
     } else if (loadHomeComponent === 'about') {
       return (<AboutUs />)
     } else if (loadHomeComponent === 'contact') {
-      return (<h1>Contact Us</h1>)
+      return (<ContactUs />)
     } else  if (loadHomeComponent === 'home') {
       return (<HomePage />)
     } else if (loadHomeComponent === 'login') {
---
+++ src/components/ContactUs.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './ContactUs.css'
+
+const ContactUs = () => {
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+
+  return (
+    <div className='contactUs'>
+      <h1>Contact Us</h1>
+      <form className='contactForm' onSubmit={handleSubmit}>
+        <label htmlFor='message'>Message</label>
+        <textarea name='message' id='message' placeholder='Enter Your Message Here'></textarea><br/>
+        <div className='contactFormRow2'>
+          <div className='contactFormRow2Email'>
+            <label htmlFor='contactEmail'>Email</label><br/>
+            <input type='email' name='email' id='contactEmail' className='email'></input>
+          </div>
+          <input type='submit' className='submit'></input>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default ContactUs
