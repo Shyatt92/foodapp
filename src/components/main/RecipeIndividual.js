34b3c8281701451516c620fcbb7ee1631865d00b
Steve Hyatt <shyatt92@gmail.com>
2021-02-19 09:45:51 +0000

    Changed individual ingredient layout to match recipe scraper output

--- src/components/main/RecipeIndividual.js
+++ src/components/main/RecipeIndividual.js
@@ -7,26 +7,40 @@ import Card from 'react-bootstrap/Card'
 
 const RecipeIndividual = () => {
   const recipe = {
-    recipeName: 'Test Recipe',
-    images: [
-      'turkey-clementine-lunch-bowl-440-400-5b6264d.jpg'
+    name: 'Chicken satay salad',
+    ingredients: [
+      '1tbsp tamari',
+      '1tsp medium curry powder',
+      '¼tsp ground cumin',
+      '1 garlic clove, finely grated',
+      '1tsp clear honey',
+      '2 skinless chicken breast fillets (or use turkey breast)',
+      '1tbsp crunchy peanut butter (choose a sugar-free version with no palm oil, if possible)',
+      '1tbsp sweet chilli sauce',
+      '1tbsp lime juice',
+      'sunflower oil, for wiping the pan',
+      '2 Little Gem lettuce hearts, cut into wedges',
+      '¼ cucumber, halved and sliced',
+      '1 banana shallot, halved and thinly sliced',
+      'coriander, chopped',
+      'seeds from ½ pomegranate'
     ],
-    rating: 3,
-    category: null,
-    description: 'A tasty meal',
-    source: 'Google',
-    sourceUrl: 'https://www.google.com',
-    prepTime: 30,
-    cookTime: 30,
-    totalTime: function() {
-      return this.prepTime + this.cookTime
+    instructions: [
+      'Pour the tamari into a large dish and stir in the curry powder, cumin, garlic and honey. Mix well. Slice the chicken breasts in half horizontally to make 4 fillets in total, then add to the marinade and mix well to coat. Set aside in the fridge for at least 1 hr, or overnight, to allow the flavours to penetrate the chicken.',
+      'Meanwhile, mix the peanut butter with the chilli sauce, lime juice, and 1 tbsp water to make a spoonable sauce. When ready to cook the chicken, wipe a large non-stick frying pan with a little oil. Add the chicken and cook, covered with a lid, for 5-6 mins on a medium heat, turning the fillets over for the last min, until cooked but still moist. Set aside, covered, to rest for a few mins.',
+      'While the chicken rests, toss the lettuce wedges with the cucumber, shallot, coriander and pomegranate, and pile onto plates. Spoon over a little sauce. Slice the chicken, pile on top of the salad and spoon over the remaining sauce. Eat while the chicken is still warm.'
+    ],
+    tags: [],
+    time: {
+      prep: '15 mins',
+      cook: '5 mins10 mins',
+      active: '',
+      inactive: '',
+      ready: '',
+      total: ''
     },
-    servings: 4,
-    difficulty: 'Easy',
-    method: '',
-    ingredients: {},
-    notes: '',
-    nutrition: {}
+    servings: '2',
+    image: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chicken-satay-salad-8f5b068.jpg'
   }
 
   return(
@@ -34,28 +48,22 @@ const RecipeIndividual = () => {
       <Col>
         <Row className='my-3'>
           <Col className='col-2'>
-            <Image src={recipe.images[0]} thumbnail/>
+            <Image src={recipe.image} thumbnail/>
           </Col>
           <Col className='col-10'>
             <Card>
               <Card.Body>
-                <Card.Title as='h1'>{recipe.recipeName}{recipe.rating ? (<span className='small'> - {recipe.rating}/5</span>): null}</Card.Title>
-                <Card.Subtitle>{recipe.description}</Card.Subtitle>
-                <p className='small'>Source: {recipe.source}<br/>
-                Source Url: <a href={recipe.sourceUrl} target='_blank' rel='noreferrer'>{recipe.sourceUrl}</a></p>
+                <Card.Title as='h1'>{recipe.name}</Card.Title>
                 <div>
                   <Row>
                     <Col>
-                      <Card body>Prep Time: {recipe.prepTime}mins</Card>
+                      <Card body>Prep Time: {recipe.time.prep}</Card>
                     </Col>
                     <Col>
-                      <Card body>Cook Time: {recipe.cookTime}mins</Card>
+                      <Card body>Cook Time: {recipe.time.cook}</Card>
                     </Col>
                     <Col>
                       <Card body>Servings: {recipe.servings}</Card>
-                    </Col>
-                    <Col>
-                      <Card body>Difficulty: {recipe.difficulty}</Card>
                     </Col>
                   </Row>
                 </div>
@@ -68,7 +76,11 @@ const RecipeIndividual = () => {
             <Card>
               <Card.Body>
                 <Card.Title>Method</Card.Title>
-                {recipe.method}
+                {recipe.instructions.map((step, keyIndex) => {
+                  return (
+                    <p key={`step-${keyIndex}`}>{step}</p>
+                  )
+                })}
               </Card.Body>
             </Card>
           </Col>
